49668a2047467e187bed63ea7f006f3ce2ed7877
308 <308@academicos.cesde.edu.co>
2022-09-15 08:28:01 -0500

    NTJ7 Se crean las condiciones del menu

---
+++ main.py
@@ -0,0 +1,15 @@
+#Hacer un menu 
+
+""" 3.Construir un programa para ir de compras en un supermercado
+que permita la construcción del siguiente MENU:
+1. Digitar 1 para saludar 
+2. Digitar 2 para pregunatr qué desayunó y dejar que diga lo que desayunó
+3. Digitar 3 para salir del menú 
+ """
+
+opcion=100
+
+print("1. Digitar 1 para saludar ")
+print("2. Digitar 2 para pregunatr qué desayunó y dejar que diga lo que desayunó")
+print("3. 3. Digitar 3 para salir del menú ")
+
